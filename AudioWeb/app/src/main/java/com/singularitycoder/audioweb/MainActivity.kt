ea5fc235e9cc75788b149c8739a0e59d699d1677
hithesh vurjana <hitheshvurjana@hitheshs-MacBook-Air.local>
2022-07-23 15:47:19 +0530

    First commit

---
+++ AudioWeb/app/src/main/java/com/singularitycoder/audioweb/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.singularitycoder.audioweb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

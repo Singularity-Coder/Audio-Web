d6cce23069f4e76cd0f21b81c69e285e462c4b91
hithesh vurjana <hitheshvurjana@hitheshs-MacBook-Air.local>
2022-07-24 17:32:26 +0530

    clean up

--- AudioWeb/app/src/main/java/com/singularitycoder/audioweb/GeneralUtils.kt
+++ AudioWeb/app/src/main/java/com/singularitycoder/audioweb/GeneralUtils.kt
@@ -3,15 +3,19 @@ package com.singularitycoder.audioweb
 import android.content.Context
 import android.content.res.Resources
 import android.graphics.Point
+import android.util.Log
 import android.view.View
 import com.google.android.material.snackbar.BaseTransientBottomBar
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers.Default
+import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.Dispatchers.Main
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import org.jsoup.Jsoup
 import java.io.File
+import java.io.IOException
 
 const val WORKER_TAG_WEB_PAGE_PARSER = "WORKER_TAG_WEB_PAGE_PARSER"
 
@@ -27,7 +31,7 @@ fun View.showSnackBar(
     anchorView: View? = null,
     duration: Int = Snackbar.LENGTH_SHORT,
     actionBtnText: String = "NA",
-    action: () -> Unit = {}
+    action: () -> Unit = {},
 ) {
     Snackbar.make(this, message, duration).apply {
         this.animationMode = BaseTransientBottomBar.ANIMATION_MODE_SLIDE
@@ -78,7 +82,7 @@ fun Context.externalFilesDir(
 inline fun deleteAllFilesFrom(
     directory: File?,
     withName: String,
-    crossinline onDone: () -> Unit = {}
+    crossinline onDone: () -> Unit = {},
 ) {
     CoroutineScope(Default).launch {
         directory?.listFiles()?.forEach files@{ it: File? ->
@@ -89,5 +93,45 @@ inline fun deleteAllFilesFrom(
         }
 
         withContext(Main) { onDone.invoke() }
+    }
+}
+
+suspend fun parseHtml(): List<WebPage> = withContext(IO) {
+    return@withContext try {
+        val webPageList = ArrayList<WebPage>()
+        val url = "https://www.example.com/"
+        val doc = Jsoup.connect(url).get()
+        val data = doc.select("class_name")
+        println("data: $data")
+        for (i in 0 until data.size) {
+            val imgUrl = data.select("class_name")
+                .select("img")
+                .eq(i)
+                .attr("src")
+            val title = data.select("h4.class_name")
+                .select("span")
+                .eq(i)
+                .text()
+            val detailUrl = data.select("h4.class_name")
+                .select("a")
+                .eq(i)
+                .attr("href")
+            println("""
+                imgUrl: $imgUrl
+                title: $title
+            """.trimIndent())
+            webPageList.add(
+                WebPage(
+                    imageUrl = imgUrl,
+                    title = title,
+                    pageUrl = "",
+                    description = ""
+                )
+            )
+        }
+        return@withContext webPageList
+    } catch (e: IOException) {
+        e.printStackTrace()
+        emptyList()
     }
 }
